836277d6e8d0654cd042cbfbaf8bf5ccf14ecd87
arky-arky <61799378+arky-arky@users.noreply.github.com>
2021-08-19 14:58:24 +0300

    Create wrapper.lua

---
+++ wrapper.lua
@@ -0,0 +1,59 @@
+-- Teohook wrapper v0.1 made by [arky#0086]
+-- License: MIT
+removeHooks() -- to prevent crashes, probably removing when 2.0 th
+local nopulldialog = false
+
+-- Functions
+
+function hit(x, y)
+    pkt = {}
+    pkt.type = 3
+    pkt.int_data = 18
+    pkt.int_x = x
+    pkt.int_y = y
+    pkt.pos_x = getLocal().pos.x
+    pkt.pos_y = getLocal().pos.y
+    sleep(100)
+    sendPacketRaw(false, pkt)
+end
+
+function place(x, y, id)
+    pkt = {}
+    pkt.type = 3
+    pkt.int_data = id
+    pkt.int_x = x
+    pkt.int_y = y
+    pkt.pos_x = getLocal().pos.x
+    pkt.pos_y = getLocal().pos.y
+    sleep(100)
+    sendPacketRaw(false, pkt)
+end
+
+function pull(name)
+    for _,player in pairs(getPlayers()) do
+        if player.name:find(name) then -- if player name contains string pull that player
+            nopulldialog = true
+            sendPacket(false, "action|dialog_return\ndialog_name|popup\nnetID|"..tostring(player.netid).."|\nnetID|"..tostring(player.netid).."|\nbuttonClicked|pull",2)
+            sleep(170)
+            nopulldialog = false
+        end
+    end
+end
+
+function joinworld(name)
+    sendPacket(false, "action|join_request\nname|"..name, 3)
+end
+
+function joinworldwithid(name, doorid)
+    sendPacket(false, "action|join_request\nname|"..name.."|"..doorid, 3)
+end
+
+-- Hooks
+
+function pullhook(varlist)
+    if varlist[1]:find("add_button|report_player|") and nopulldialog == true then
+        return true
+    end
+end
+addHook("OnVarlist", "pull hook", pullhook)
+
